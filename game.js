d28101b62bc16ab11ecab0f4d69c875c5d7a06a2
Sungmiii <christyksmile@gmail.com>
2018-08-12 20:57:49 +1200

    count is working

--- game.js
+++ game.js
@@ -39,9 +39,18 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
-  
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  var total = document.getElementsByClassName('board')[0].children;
+  for (var i =0; i<total.length; i++){
+    if(total[i].classList.contains('blue')){
+      totals.blue++;
+  } else if(total[i].classList.contains('green')){
+    totals.green++;
+  } else if(total[i].classList.contains('invisible')){
+    totals.invisible++;
+  }
+  
+  }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
